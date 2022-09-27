0d783c368123d494e094fc58c0abb1ae12b946a2
Sergei <msvp@mail.ru>
2022-09-27 21:07:41 +0300

    ИТОГОВЫЙ ПРОЕКТ 6.9 (HW-03) Messenger

---
+++ protect/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'protect/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context.update({
+        # })
+        return context
